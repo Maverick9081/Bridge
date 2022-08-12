72286ef3907d6d2bbc930ce083b4768c411bdbca
Maverick9081 <faliyaabhishek@gmail.com>
2022-08-12 10:06:16 +0530

    config account closing

--- programs/Bridge/src/lib.rs
+++ programs/Bridge/src/lib.rs
@@ -1,72 +1,69 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, CloseAccount, Mint, SetAuthority, TokenAccount, Transfer};
+use anchor_spl::token::{ self, CloseAccount, Mint, SetAuthority, TokenAccount, Transfer };
 use spl_token::instruction::AuthorityType;
 
-declare_id!("BJ1Lgyz2R9oqQuUYShkuaNgGf1ZKoeaga2LsgJeuF5zE");
+declare_id!("8SdY5Ysr3FMonohSeS1DNRnknviN7cVwp26ZwDj5ido1");
 
 #[program]
 pub mod bridge {
     use super::*;
     const ESCROW_PDA_SEED: &[u8] = b"escrow";
-    pub fn freeze_token(ctx: Context<FreezeToken>, amount: u64,chain_id : u8,eth_address : String) ->Result<()> {
-
+    pub fn freeze_token(
+        ctx: Context<FreezeToken>,
+        amount: u64,
+        chain_id: u8,
+        eth_address: String
+    ) -> Result<()> {
         ctx.accounts.freezing_config.mint = ctx.accounts.mint.to_account_info().key();
         ctx.accounts.freezing_config.sender = ctx.accounts.sender.to_account_info().key();
-        
-         let (vault_authority, _vault_authority_bump) =
-            Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
+
+        let (vault_authority, _vault_authority_bump) = Pubkey::find_program_address(
+            &[ESCROW_PDA_SEED],
+            ctx.program_id
+        );
 
         token::set_authority(
             ctx.accounts.into_set_authority_context(),
             AuthorityType::AccountOwner,
-            Some(vault_authority),
+            Some(vault_authority)
         )?;
-        
-          token::transfer(
-            ctx.accounts.into_transfer_to_pda_context(),
-            amount,
-        )?;
+
+        token::transfer(ctx.accounts.into_transfer_to_pda_context(), amount)?;
 
         emit!(Freeze {
             chain_id: chain_id,
-            sender : ctx.accounts.sender.key(),
-            mint : ctx.accounts.mint.key()
+            sender: ctx.accounts.sender.key(),
+            mint: ctx.accounts.mint.key(),
         });
-        msg!("{}",&eth_address);
+        msg!("ETH address :{}", &eth_address);
 
         Ok(())
     }
 
-
-    pub fn release_token(ctx: Context<ReleaseToken>, amount: u64) ->Result<()> {
-
+    pub fn release_token(ctx: Context<ReleaseToken>, amount: u64) -> Result<()> {
         if ctx.accounts.freezing_config.sender != ctx.accounts.receiver.key() {
-          return err!(EscrowError::IncorrectOwner)
+            return err!(EscrowError::IncorrectOwner);
         }
-        const ESCROW_PDA_SEED: &[u8] = b"escrow";
-        let (_vault_authority, vault_authority_bump) =
-            Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
+        let (_vault_authority, vault_authority_bump) = Pubkey::find_program_address(
+            &[ESCROW_PDA_SEED],
+            ctx.program_id
+        );
 
         let authority_seeds = &[&ESCROW_PDA_SEED[..], &[vault_authority_bump]];
 
         token::transfer(
-            ctx.accounts
-                .into_transfer_to_receiver_context()
-                .with_signer(&[&authority_seeds[..]]),
+            ctx.accounts.into_transfer_to_receiver_context().with_signer(&[&authority_seeds[..]]),
             amount
         )?;
 
         token::close_account(
-            ctx.accounts
-                .into_close_context()
-                .with_signer(&[&authority_seeds[..]]),
+            ctx.accounts.into_close_context().with_signer(&[&authority_seeds[..]])
         )?;
 
         emit!(Release {
-            receiver : ctx.accounts.receiver.key(),
-            mint : ctx.accounts.mint.key()
+            receiver: ctx.accounts.receiver.key(),
+            mint: ctx.accounts.mint.key(),
         });
- 
         Ok(())
     }
 }
@@ -74,7 +71,7 @@ pub mod bridge {
 #[derive(Accounts)]
 pub struct FreezeToken<'info> {
     ///CHECK : Not dangerous
-   #[account(mut, signer)]
+    #[account(mut, signer)]
     pub sender: AccountInfo<'info>,
     #[account(mut)]
     pub sender_ata: Account<'info, TokenAccount>,
@@ -82,30 +79,22 @@ pub struct FreezeToken<'info> {
 
     #[account(
         init,
-        seeds = 
-        [   
-            b"vault".as_ref(),
-            mint.key().as_ref()
-        ],
+        seeds = [b"vault".as_ref(), mint.key().as_ref()],
         bump,
         payer = sender,
         token::mint = mint,
-        token::authority = sender,
+        token::authority = sender
     )]
     pub vault_account: Account<'info, TokenAccount>,
     ///CHECK : Not dangerous
     #[account(
         init,
-        seeds = 
-        [   
-            b"config".as_ref(),
-            mint.key().as_ref()
-        ],
+        seeds = [b"config".as_ref(), mint.key().as_ref()],
         bump,
         payer = sender,
         space = 90
     )]
-    pub freezing_config : Account<'info,FreezingConfig>,
+    pub freezing_config: Account<'info, FreezingConfig>,
     ///CHECK : Not dangerous
     pub token_program: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
@@ -127,9 +116,11 @@ pub struct ReleaseToken<'info> {
     pub vault_account: Box<Account<'info, TokenAccount>>,
     /// CHECK: This is not dangerous because we don't read or write from this account
     pub vault_authority: AccountInfo<'info>,
-    
+
     ///CHECK : Not dangerous
     #[account(
+        mut,
+        close = sender,
         seeds = 
         [   
             b"config".as_ref(),
@@ -137,7 +128,8 @@ pub struct ReleaseToken<'info> {
         ],
         bump,
     )]
-    pub freezing_config : Account<'info,FreezingConfig>,
+    pub freezing_config: Account<'info, FreezingConfig>,
+
     ///CHECK : Not dangerous
     pub token_program: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
@@ -147,37 +139,33 @@ pub struct ReleaseToken<'info> {
 #[event]
 pub struct Freeze {
     pub chain_id: u8,
-    pub sender : Pubkey,
-    pub mint : Pubkey,
+    pub sender: Pubkey,
+    pub mint: Pubkey,
 }
 
 #[event]
 pub struct Release {
-    pub receiver : Pubkey,
-    pub mint : Pubkey,
+    pub receiver: Pubkey,
+    pub mint: Pubkey,
 }
 
 #[account]
 pub struct FreezingConfig {
-    pub mint : Pubkey,
-    pub sender : Pubkey,
+    pub mint: Pubkey,
+    pub sender: Pubkey,
 }
 
 #[error_code]
-pub enum EscrowError{
-  // 6003
-  #[msg("IncorrectOwner")]
-  IncorrectOwner,
+pub enum EscrowError {
+    // 6003
+    #[msg("IncorrectOwner")]
+    IncorrectOwner,
 }
 
 impl<'info> FreezeToken<'info> {
-   
     fn into_transfer_to_pda_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
         let cpi_accounts = Transfer {
-            from: self
-                .sender_ata
-                .to_account_info()
-                .clone(),
+            from: self.sender_ata.to_account_info().clone(),
             to: self.vault_account.to_account_info().clone(),
             authority: self.sender.clone(),
         };
@@ -194,7 +182,6 @@ impl<'info> FreezeToken<'info> {
 }
 
 impl<'info> ReleaseToken<'info> {
-
     fn into_transfer_to_receiver_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
         let cpi_accounts = Transfer {
             from: self.vault_account.to_account_info().clone(),
@@ -204,7 +191,7 @@ impl<'info> ReleaseToken<'info> {
         CpiContext::new(self.token_program.clone(), cpi_accounts)
     }
 
-     fn into_close_context(&self) -> CpiContext<'_, '_, '_, 'info, CloseAccount<'info>> {
+    fn into_close_context(&self) -> CpiContext<'_, '_, '_, 'info, CloseAccount<'info>> {
         let cpi_accounts = CloseAccount {
             account: self.vault_account.to_account_info().clone(),
             destination: self.sender.clone(),
--- tests/Bridge.ts
+++ tests/Bridge.ts
@@ -2,21 +2,130 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { BN } from "bn.js";
 import { Bridge } from "../target/types/bridge";
+import {Keypair, SystemProgram, Transaction,Connection, clusterApiUrl } from "@solana/web3.js";
+import NodeWallet from '@project-serum/anchor/dist/cjs/nodewallet';
+import { getAssociatedTokenAddress, TOKEN_PROGRAM_ID, MintLayout, AccountLayout,getMinimumBalanceForRentExemptMint, createMintToInstruction, createInitializeMintInstruction, createMint, mintTo, createAccount } from "@solana/spl-token";
 
 describe("Bridge", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
+  const commitment= 'processed';
+  const connection = new Connection(clusterApiUrl('devnet'));
+  
+  const options = anchor.AnchorProvider.defaultOptions();
+  const wallet = NodeWallet.local();
+  const provider = new anchor.AnchorProvider(connection, wallet, options);
+
   const program = anchor.workspace.Bridge as Program<Bridge>;
 
   it("Is initialized!", async () => {
     // Add your test here.
-    let amount = new BN(1);
-    const tx = await program.methods.freezeToken(amount,{
-      accounts:{
 
-      }
-    })
-    console.log("Your transaction signature", tx);
+    // let mint =  Keypair.generate();
+    // let tx1 = new Transaction().add(
+    //   // create mint account
+    //   SystemProgram.createAccount({
+    //     fromPubkey: program.provider.publicKey,
+    //     newAccountPubkey: mint.publicKey,
+    //     space: MintLayout.span,
+    //     lamports: await getMinimumBalanceForRentExemptMint(program.provider.connection),
+    //     programId: TOKEN_PROGRAM_ID,
+    //   }),
+      // init mint account
+    //   createInitializeMintInstruction(
+    //      // always TOKEN_PROGRAM_ID
+    //     mint.publicKey, // mint pubkey
+    //     0, // decimals
+    //     program.provider.publicKey, // mint authority
+    //     program.provider.publicKey // freeze authority (if you don't need it, you can set `null`)
+    //   )
+    // );
+    const payer = anchor.web3.Keypair.generate();
+
+
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(payer.publicKey, 1000000000),
+      "processed"
+    );
+
+    let mint = await createMint(
+      provider.connection,
+      payer,
+      payer.publicKey,
+      null,
+      0,
+    );
+    console.log('10')
+
+    let senderAta = await createAccount(connection,payer,mint,payer.publicKey);
+
+    await mintTo(connection,payer,mint,payer.publicKey,payer.publicKey,1);
+
+
+
+    console.log('1')
+  
+
+    // await provider.sendAndConfirm(tx1,[mint])
+    // console.log('1')
+
+    // const senderAta = await getAssociatedTokenAddress(mint.publicKey,program.provider.publicKey);
+
+    // let tx2 = new Transaction().add(
+    //   createMintToInstruction(
+    //      // always TOKEN_PROGRAM_ID
+    //     mint.publicKey, // mint
+    //     senderAta, // receiver (sholud b a token account)
+    //     program.provider.publicKey, // mint authority
+    //     1 ,
+    //     // amount. if your decimals is 8, you mint 10^8 for 1 token.
+    //   )
+    // );
+
+    // console.log(tx2)
+
+    
+//  await provider.sendAndConfirm(tx2,[])
+
+    
+ console.log('1')
+    
+
+    const vaultAccount = await anchor.web3.PublicKey.findProgramAddress([
+      Buffer.from("vault"),
+      mint.publicKey.toBuffer()
+      ],
+      program.programId
+  )
+ 
+  const freezingConfig = await anchor.web3.PublicKey.findProgramAddress([
+      Buffer.from("config"),
+      mint.publicKey.toBuffer()
+      ],
+      program.programId
+  )
+  const tokenProgram = new anchor.web3.PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
+  const systemProgram = new anchor.web3.PublicKey('11111111111111111111111111111111');
+  const rent = new anchor.web3.PublicKey("SysvarRent111111111111111111111111111111111");
+
+
+
+
+
+
+    let amount = new anchor.BN(1);
+    const freeze =  await program.methods.freezeToken(amount,2,'hello').accounts({
+      sender :program.provider.publicKey,
+       senderAta :senderAta,
+       mint :mint.publicKey,
+       vaultAccount :vaultAccount[0],
+       freezingConfig :freezingConfig[0],
+       tokenProgram :tokenProgram,
+       systemProgram : systemProgram, 
+       rent :rent
+    }
+  ).rpc();
+    console.log("Your transaction signature", freeze);
   });
 });
